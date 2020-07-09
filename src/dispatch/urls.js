d0870392f6b386905884264a433a87598b4e2024
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-07-08 19:29:32 -0700

    chore: add heroku port env

--- src/dispatch/urls.js
+++ src/dispatch/urls.js
@@ -6,7 +6,7 @@ const getUrls = () => {
   // console.log(process.env.NODE_ENV);
   const sentryDSN = () => process.env.REACT_APP_SENTRY_DSN;
   const nodeEnv = process.env.NODE_ENV || "development";
-  const localPort = process.env.REACT_APP_PORT_FE || 3000;
+  const localPort = process.env.REACT_APP_PORT_FE || process.env.PORT || 3000;
   const urlBases = {
     development: process.env.REACT_APP_BASE_DEVELOPMENT,
     staging: process.env.REACT_APP_BASE_STAGING,
